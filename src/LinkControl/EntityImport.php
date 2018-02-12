c286a397cc0329b1e4ab7a07e35da6e27c6631be
Edinei <galera.org@gmail.com>
2018-02-12 09:32:11 -0200

    entity import and delete after

--- src/LinkControl/EntityImport.php
+++ src/LinkControl/EntityImport.php
@@ -17,12 +17,16 @@ class EntityImport
         if (file_exists(PATH_HOME . "vendor/conn/{$lib}/entity/cache/info")) {
             foreach (Helper::listFolder(PATH_HOME . "vendor/conn/{$lib}/entity/cache") as $file) {
                 if ($file !== "info" && preg_match('/\w+\.json$/i', $file)) {
-                    if (file_exists(PATH_HOME . "entity/cache/{$file}"))
-                        return false;
+                    if (!file_exists(PATH_HOME . "entity/cache/{$file}"))
+                        $this->importEntity($file, $lib);
 
-                    $this->importEntity($file, $lib);
+                    unlink(PATH_HOME . "vendor/conn/{$lib}/entity/cache/{$file}");
+                    unlink(PATH_HOME . "vendor/conn/{$lib}/entity/cache/info/{$file}");
                 }
             }
+            rmdir(PATH_HOME . "vendor/conn/{$lib}/entity/cache/info");
+            rmdir(PATH_HOME . "vendor/conn/{$lib}/entity/cache");
+            rmdir(PATH_HOME . "vendor/conn/{$lib}/entity");
         }
     }
 
