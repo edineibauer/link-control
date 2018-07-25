64d7cc73a379301b10e3725df6027acd882c69a8
Edinei <galera.org@gmail.com>
2018-07-25 11:30:08 -0300

    updates

---
+++ view/request/post.php
@@ -0,0 +1,13 @@
+<?php
+use \Helpers\Check;
+use \LinkControl\Route;
+if (Check::ajax()) {
+    $lib = strip_tags(trim(filter_input(INPUT_POST, "lib", FILTER_DEFAULT)));
+    $url = strip_tags(trim(filter_input(INPUT_POST, "file", FILTER_DEFAULT)));
+
+    $include = PATH_HOME . ($lib !== DOMINIO ? VENDOR . "{$lib}/" : "") . "ajax/{$url}.php";
+    if(file_exists($include))
+        include_once $include;
+    else
+        $data = "";
+}
